c9dc7a1ea7551935c9fcbf4e8ab063cc4852a525
Anand Kumar <hyeiamaka06@gmail.com>
2024-02-25 18:17:10 +0530

    first commit

--- lib/repository/database.dart
+++ lib/repository/database.dart
@@ -4,46 +4,46 @@ import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 
 // Singleton class
-class DatabaseRepository{
-static const _databaseName = 'places.db';
-static const _databaseVersion = 1;
+class DatabaseRepository {
+  static const _databaseName = 'places.db';
+  static const _databaseVersion = 1;
 
-static const placeTable = 'user_place';
-static const columnId = 'id';
-static const columnTitle = 'title';
-static const columnImage = 'image';
-static const columnLat = 'lat';
-static const columnLang = 'lng';
-static const columnAddress = 'address';
+  static const placeTable = 'user_place';
+  static const columnId = 'id';
+  static const columnTitle = 'title';
+  static const columnImage = 'image';
+  static const columnLat = 'lat';
+  static const columnLang = 'lng';
+  static const columnAddress = 'address';
 
-static DatabaseRepository ? _databaseRepository;
+  static DatabaseRepository? _databaseRepository;
 // a private named to prevent the class from being instantiated outside the class.
-DatabaseRepository._createInstance();
+  DatabaseRepository._createInstance();
 
 // to create only one instance
-factory DatabaseRepository (){
-  _databaseRepository ??= DatabaseRepository._createInstance();
-  return _databaseRepository!;
-}
+  factory DatabaseRepository() {
+    _databaseRepository ??= DatabaseRepository._createInstance();
+    return _databaseRepository!;
+  }
 
- // only have a single app-wide reference to the database
-  static Database  ? _database;
+  // only have a single app-wide reference to the database
+  static Database? _database;
   Future<Database> get database async {
-  _database ??= await initializeDatabase();
-  return _database!;
-	}
-  
+    _database ??= await initializeDatabase();
+    return _database!;
+  }
+
   Future<Database> initializeDatabase() async {
-		Directory directory = await getApplicationDocumentsDirectory(); 
-		String path = '${directory.path}$_databaseName';
-		var placesDatabase = await openDatabase(path, version: _databaseVersion, onCreate: _createDb);
-		return placesDatabase;
+    Directory directory = await getApplicationDocumentsDirectory();
+    String path = '${directory.path}$_databaseName';
+    var placesDatabase = await openDatabase(path,
+        version: _databaseVersion, onCreate: _createDb);
+    return placesDatabase;
   }
 
   // Create Database
   void _createDb(Database db, int newVersion) async {
-  await db.execute(
-      '''
+    await db.execute('''
        CREATE TABLE $placeTable(
        $columnId TEXT PRIMARY KEY, 
        $columnTitle TEXT,
@@ -56,19 +56,19 @@ factory DatabaseRepository (){
   }
 
   // Insert Places
-  Future<void> insertPlaces (PlaceModel places) async {
+  Future<void> insertPlaces(PlaceModel places) async {
     Database db = await database;
     await db.insert(placeTable, places.toJson());
   }
 
   // getPlaces
-  Future<List<PlaceModel>> getPlaceList() async{
-  Database db = await database;
-  var result = await db.query(placeTable);
-  List<PlaceModel> places = [];
-  for(int i=0;i<result.length;i++) {
-    places.add(PlaceModel.fromMapObject(result[i]));
-  }
-  return places;
+  Future<List<PlaceModel>> getPlaceList() async {
+    Database db = await database;
+    var result = await db.query(placeTable);
+    List<PlaceModel> places = [];
+    for (int i = 0; i < result.length; i++) {
+      places.add(PlaceModel.fromMapObject(result[i]));
+    }
+    return places;
   }
 }
